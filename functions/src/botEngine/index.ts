0b194de6d0b52f79527b34b443d60f39e43ce01b
AdiY <YDdevelops@gmail.com>
2025-07-09 16:18:13 +0300

    chore: remove functions folder from public repo

--- functions/src/botEngine/index.ts
+++
@@ -1,8 +0,0 @@
-import { Conversation, IncomingMessage, BotAction, BotState } from '../schema/types';
-import { conversationStateReducer } from './conversationStateReducer';
-
-export * from './actions';
-export { conversationStateReducer };
-
-// Re-export types for convenience
-export type { Conversation, IncomingMessage, BotAction, BotState };
